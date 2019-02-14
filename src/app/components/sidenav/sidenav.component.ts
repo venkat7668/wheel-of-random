57d0c9411f8eabf048c8306173db21c78164b385
Venkateswararao Lankalapally <vlankala@callidussoftware.com>
2019-02-15 00:08:52 +0530

    sidenav changes

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -33,7 +33,12 @@ export class SidenavComponent implements OnInit {
   }
 
   ngOnInit() {
-    //setTimeout(()=>this.addingGroup = false, 1000);
+    let $ = window['$'];
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
   }
 
   getAllGroups() {
